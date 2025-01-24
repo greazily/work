1510ef740e86128a3640d5b87ecfbaeadce48586
full-bleed <dahlen.mikael@gmail.com>
2025-01-24 13:18:52 +0100

    End

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,14 @@ function init() {
     let boundry = document.querySelector(".background__graphics");
     h = boundry.offsetHeight;
     w = boundry.offsetWidth;
-    console.log(w, h);
+    // console.log(w, h);
     clearInterval(interval);
     interval = setInterval(randomCycle, 6000);
 };
 
 function randomCycle() {
     let graphics = document.querySelectorAll(".graphic__element");
-    console.log(graphics);
+    // console.log(graphics);
 
        
     graphics.forEach((graphic, index) => {
@@ -20,9 +20,9 @@ function randomCycle() {
 
             graphic.style.left = Math.floor(Math.random() * w)  - gw /2 + "px";
             graphic.style.top = Math.floor(Math.random() * h) - gh /2 + "px";
-            graphic.style.opacity = Math.floor(Math.random() * 2);
+            graphic.style.opacity = 1;
 
-        }, index * 1000)
+        }, index * 500)
         
     })
 
--- assets/js/list.js
+++ assets/js/list.js
@@ -1,75 +1,116 @@
-gsap.registerPlugin(Flip);
+// gsap.registerPlugin(Flip);
+// let projects = document.querySelectorAll(".project"),
+//     previews = document.querySelectorAll(".preview__clip"),
+//     bar = document.getElementById("bar_moving"),
+//     index = 0;
+//     loop = setInterval(timeLine, 3000),
+//     hovering = false;
+
+
+// function descend(){
+//     projects.forEach(project => {
+//         project.classList.remove("highlight");
+//     });
+
+//     // const state = Flip.getState(bar);
+//     if(hovering === true){
+//             projects[index].prepend(bar);
+//     } else {
+//         if(index < projects.length -1) {
+//             index += 1;
+//             projects[index].prepend(bar);
+//             timeLine();
+//         } else {
+//             index = 0;
+//             projects[index].prepend(bar);
+//         };
+//     };
+//     // Flip.from(state, {
+//     //     duration: 0.5, 
+//     //     ease: "power1.inOut", 
+//     //     onComplete: ()=> {previewChanger(index), timeLine()}
+//     // });
+//     if(projects[index].contains(bar)){
+//         projects[index].classList.add("highlight");
+//     };
+
+//     previewChanger(index);
+// };
+
+// projects.forEach(project => {
+//     project.addEventListener("mouseover", ()=>{
+//         hovering = true;
+//         index = Number(project.id)
+//         hoverCheck();
+//         timeLineIn();
+//     });
+//     project.addEventListener("mouseleave", ()=>{
+//         hovering = false;
+//         loop = setInterval(descend, 3000);
+//         hoverCheck();
+//         timeLineOut();
+//     });
+// });
+
+// function hoverCheck(){
+//     if (hovering === false){
+//         loop;
+//     } else {
+//         clearInterval(loop)
+//     }
+// };
+
+// function indexer(arr){
+//     for (let i = 0; i < arr.length; i++) {
+//         arr[i].id = i;
+//     };
+// };
+
+// function previewChanger(index) {
+//         previews.forEach(clip => {
+//             clip.classList.remove("show");
+//         });
+//         previews[Number(index)].classList.add("show");
+// };
+// function timeLine(){
+//     tl = gsap.timeline();
+
+//     tl.to(".bar", {width:"100%"})
+//     tl.to(".bar", {width:"0%", onComplete: ()=> {descend()}}, ">1")
+// };
+// function timeLineIn(){
+//     tl = gsap.timeline();
+
+//     tl.to(".bar", {width:"100%"})
+// };
+// function timeLineOut(){
+//     tl = gsap.timeline();
+
+//     tl.to(".bar", {width:"0%", onComplete: ()=> {descend()}}, ">1")
+// };
+
+
+// indexer(projects);
+// // indexer(previews);
+
 let projects = document.querySelectorAll(".project"),
     previews = document.querySelectorAll(".preview__clip"),
     bar = document.getElementById("bar_moving"),
-    index = 0;
-    loop = setInterval(descend, 3000),
-    hovering = false;
+    hover = false;
 
+    tl = gsap.timeline()
 
-function descend(h){
-    let hactive = h;
+    tl.add("in")
+    tl.to(bar, {duration: 0.5, width: "100%"}, "in")
+    tl.add("out", ">+1")
+    tl.to(bar, {duration: 0.5, width: "0%"}, "out")
+
     projects.forEach(project => {
-        project.classList.remove("highlight");
-    });
-
-    const state = Flip.getState(bar);
-    if(hovering === true){
-            projects[hactive].prepend(bar);
-            index = Number(hactive);
-    } else {
-        if(index < projects.length -1) {
-            index += 1;
-            projects[index].prepend(bar);
-        } else {
-            index = 0;
-            projects[index].prepend(bar);
-        };
-    };
-    Flip.from(state, {
-        duration: 0.5, 
-        ease: "power1.inOut", 
-        onComplete: ()=> {previewChanger(index)}
-    });
-    if(projects[index].contains(bar)){
-        projects[index].classList.add("highlight");
-    };
-};
-
-projects.forEach(project => {
-    project.addEventListener("mouseover", ()=>{
-        hovering = true;
-        hoverCheck();
-        descend(project.id);
-    });
-    project.addEventListener("mouseleave", ()=>{
-        hovering = false;
-        loop = setInterval(descend, 3000);
-        hoverCheck();
-    });
-});
-
-function hoverCheck(){
-    if (hovering === false){
-        loop;
-    } else {
-        clearInterval(loop)
-    }
-};
-
-function indexer(arr){
-    for (let i = 0; i < arr.length; i++) {
-        arr[i].id = i;
-    };
-};
-
-function previewChanger(index) {
-        previews.forEach(clip => {
-            clip.classList.remove("show");
-        });
-        previews[Number(index)].classList.add("show");
-}
-
-
-indexer(projects);
-// indexer(previews);
+        project.addEventListener("mouseover", ()=>{
+            tl.addPause("out")
+            console.log(project)
+        })
+        project.addEventListener("mouseleave", ()=>{
+            tl.resume("out")
+        })
+    })
